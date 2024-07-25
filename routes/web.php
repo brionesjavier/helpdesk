1138492c718f1046d2ddeedc27f12da36ea7e66a
javier briones <javier.brms@gmail.com>
2024-07-25 19:04:12 -0400

    se creo la tabla de categorias, se configuro las rutas a la tabla,se creo el model, y de creo estructura basica de controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategorieController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+route::resource('categories',CategorieController::class);
