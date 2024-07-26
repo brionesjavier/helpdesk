c591f96b940e5cf30d6f1e0d25cf29edd83cbba1
javier briones <javier.brms@gmail.com>
2024-07-26 16:33:26 -0400

    añadido vista editar

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,6 @@ Route::middleware('auth')->group(function () {
 Route::get('categories',[CategoryController::class,'index'])->name('categories.index');
 Route::get('categories/create',[CategoryController::class,'create'])->name('categories.create');
 Route::post('categories/create',[CategoryController::class,'store'])->name('categories.store');
+Route::get('categories/edit/{category}',[CategoryController::class,'edit'])->name('categories.edit');
+Route::post('categories/update',[CategoryController::class,'update'])->name('categories.update');
 require __DIR__.'/auth.php';
