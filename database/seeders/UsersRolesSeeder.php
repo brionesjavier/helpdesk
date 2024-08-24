5a2a061e05924db8aeb09ac47c5c47e6c7d9fc3e
javier briones <javier.brms@gmail.com>
2024-08-24 17:10:35 -0400

    user de prueba

---
+++ database/seeders/UsersRolesSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UsersRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // Verificar que los roles ya existen
+        $adminRole = Role::where('name', 'admin')->first();
+        $userRole = Role::where('name', 'user')->first();
+
+        // Crear el usuario root si no existe
+        $user = User::firstOrCreate([
+            'email' => 'root@gmail.com',
+        ], [
+            'name' => 'Root User',
+            'password' => Hash::make('password'), // Cambia por tu contraseña deseada
+        ]);
+
+        // Asignar roles al usuario
+        $user->assignRole($adminRole);
+        $user->assignRole($userRole);
+    }
+}
